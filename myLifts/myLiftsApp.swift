f708726a77a3ffd0e4636d5d14f1916250541f6a
Cory <coryandreasen@gmail.com>
2022-07-30 12:28:30 -0400

    Initial Commit

---
+++ myLifts/myLiftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myLiftsApp.swift
+//  myLifts
+//
+//  Created by Cory Andreasen on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct myLiftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
